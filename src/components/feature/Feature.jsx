fc48876aa5559c0464b8679412f58c810e16873a
axido545 <axido545@gmail.com>
2024-01-01 16:16:19 +0100

    add first feature with content

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,5 +1,17 @@
 import "./feature.css"
+import PropTypes from 'prop-types';
 
-export default function Feature() {
-    return <p>feature</p>
+export default function Feature({ title, url, content }) {
+    return <div className="feature">
+        <img className="img-feature" src={url} alt={title} />
+        <p className="title-feature">{title}</p>
+        <p>{content}</p>
+    </div>
+}
+
+Feature.propTypes = {
+    title: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,15 +3,17 @@ import Header from "../../layout/header/Header.jsx"
 import Footer from '../../layout/footer/Footer.jsx'
 import Banner from '../../components/banner/Banner.jsx'
 import Feature from '../../components/feature/Feature.jsx'
+import url1 from "../../assets/icon-chat.png"
 
 function Home() {
+  const content1 = "Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes."
 
   return (
     <>
       <Header />
       <Banner />
       <p>home page</p>
-      <Feature />
+      <Feature url={url1} title="You are our #1 priority" content={content1} />
       <Footer />
     </>
   )
