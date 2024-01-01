114065bb32078af10e450a39b38d9dc867ad2d38
axido545 <axido545@gmail.com>
2024-01-01 14:41:00 +0100

    add header

--- src/layout/header/Header.jsx
+++ src/layout/header/Header.jsx
@@ -4,5 +4,8 @@ import logo from "../../assets/argentBankLogo.png"
 export default function Header() {
     return <div className="header">
         <img className="logo" src={logo} alt="logo" />
+        <nav className="header-nav">
+            <i className="fa fa-user-circle"></i>Sign In
+        </nav>
     </div>
 }
