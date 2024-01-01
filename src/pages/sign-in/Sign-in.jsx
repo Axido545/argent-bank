5ea19509ee1e72f5e8a279fe5f33cd21b1bd8a68
axido545 <axido545@gmail.com>
2024-01-01 18:54:25 +0100

    correction sign-in

--- src/pages/sign-in/Sign-in.jsx
+++ src/pages/sign-in/Sign-in.jsx
@@ -1,11 +1,11 @@
-import Header from "../../layout/header/Header"
-import Footer from "../../layout/footer/Footer"
-import "./sing-in.css"
+import Header from "../../layout/header/Header";
+import Footer from "../../layout/footer/Footer";
+import "./sign-in.css"
 
 export default function SignIn() {
     return <div>
         <Header />
-        <main className="main-sing-in">
+        <main className="main-sign-in">
             <form className="form-content">
                 <i className="fa fa-user-circle sign-in-icon"></i>
                 <h1>Sign in</h1>
