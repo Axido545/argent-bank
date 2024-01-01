390dc621f3f97436d6bbfbf998e16a4764ac6028
axido545 <axido545@gmail.com>
2024-01-01 15:10:56 +0100

    add text banner

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -2,7 +2,14 @@ import "./banner.css"
 
 export default function Banner() {
     return <div className="banner">
-
+        <section className="content-banner">
+            <p className="title-banner">
+                No fees <br />
+                No minimum deposit.<br />
+                High interest rates.<br />
+            </p>
+            <p className="text-banner">Open a savings account with Argent Bank today!</p>
+        </section>
     </div>
 
 
