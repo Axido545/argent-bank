3b754afcb0676198ba6b6eaf8e084ef63272ee74
axido545 <axido545@gmail.com>
2024-01-01 15:16:22 +0100

    add empty feature component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import './home.css'
 import Header from "../../layout/header/Header.jsx"
 import Footer from '../../layout/footer/Footer.jsx'
 import Banner from '../../components/banner/Banner.jsx'
+import Feature from '../../components/feature/Feature.jsx'
 
 function Home() {
 
@@ -10,6 +11,7 @@ function Home() {
       <Header />
       <Banner />
       <p>home page</p>
+      <Feature />
       <Footer />
     </>
   )
