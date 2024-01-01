98283bf06d5ef45f8e3fca3389636f1d0683f252
axido545 <axido545@gmail.com>
2024-01-01 15:01:51 +0100

    add banner img

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import './home.css'
 import Header from "../../layout/header/Header.jsx"
 import Footer from '../../layout/footer/Footer.jsx'
+import Banner from '../../components/banner/Banner.jsx'
 
 function Home() {
 
   return (
     <>
       <Header />
+      <Banner />
       <p>home page</p>
       <Footer />
     </>
