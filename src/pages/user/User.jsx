6baf6e405a8ab036d11be4d336e350a4feebcb1b
axido545 <axido545@gmail.com>
2024-01-01 23:10:15 +0100

    add user page with account component

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -1,11 +1,21 @@
 import Header from "../../layout/header/Header"
 import Footer from "../../layout/footer/Footer"
 import "./user.css"
+import Account from "../../components/account/Account"
 
 export default function User() {
     return <div>
         <Header />
-        <p>User page</p>
+        <main className="user-main">
+            <header className="user-header">
+                <h1>Welcome back<br /> Tony Jarvis !</h1>
+                <button className="user-edit-button">Edit Name</button>
+            </header>
+            <div className="section-account">
+                <Account title="Argent Bank Savings (x6712)" amount="$10,928.42" description="Available Balance" />
+
+            </div>
+        </main>
         <Footer />
     </div>
 }
