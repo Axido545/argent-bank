cc4096ca2d32f762515163cbc3783d02c513cac1
axido545 <axido545@gmail.com>
2024-01-04 23:33:05 +0100

    add authSlice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./authSlice"
+
 
 export const mainStore = configureStore({
     reducer: {
+        auth: authReducer
     },
 });
