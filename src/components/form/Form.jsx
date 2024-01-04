532d1285ed5aff2e119a68eb0058f3fcc69676df
axido545 <axido545@gmail.com>
2024-01-05 00:13:14 +0100

    api error 404

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,17 +1,29 @@
 import "./form.css"
+import { loginAsync } from "../../redux/authSlice"
+import { useDispatch } from "react-redux";
+import { useState } from "react";
+
 export default function Form() {
+    const dispatch = useDispatch()
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('test')
+        dispatch(loginAsync({ email, password }));
+    };
+
     return <form className="form-content">
         <i className="fa fa-user-circle login-icon"></i>
         <h1 className="form-title">Sign in</h1>
         <div className="input-wrapper">
-            <label htmlFor="username">Username</label>
-            <input name="username" />
-        </div>
+            <label htmlFor="email">Email</label>
+            <input name="email" value={email} onChange={(e) => setEmail(e.target.value)} />        </div>
         <div className="input-wrapper">
             <label htmlFor="password">Password</label>
 
-            <input name="password" />
-        </div>
+            <input name="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />        </div>
 
         <div className="input-remember">
             <input
@@ -21,7 +33,7 @@ export default function Form() {
             /><label htmlFor="remember-me">Remember me</label>
 
         </div>
-        <a className="login-button" href="/dashboard">Sign in</a>
+        <button onClick={handleSubmit} className="login-button" href="/dashboard">Sign in</button>
     </form>
 
 }
--- src/redux/authSlice.js
+++ src/redux/authSlice.js
@@ -1,4 +1,10 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { getLogin } from "../services/api";
+
+export const loginAsync = createAsyncThunk("auth/login", async ({ email, password }) => {
+    const response = await getLogin(email, password);
+    return response.body;
+});
 
 const authSlice = createSlice({
     name: "auth",
