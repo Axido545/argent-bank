5b7183691e9cca3a984f25e9eec5a806c0ccd472
axido545 <axido545@gmail.com>
2024-01-05 22:35:27 +0100

    add mockup template de user

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -12,8 +12,13 @@ export default function Dashboard() {
                 <button className="user-edit-button">Edit Name</button>
             </header>
             <div className="section-account">
+                <Account title="Argent Bank Checking (x8349)" amount="$2,082.79" description="Available Balance" />
+            </div>
+            <div className="section-account">
                 <Account title="Argent Bank Savings (x6712)" amount="$10,928.42" description="Available Balance" />
-
+            </div>
+            <div className="section-account">
+                <Account title="Argent Bank Credit Card (x8349)" amount="$184.30" description="Current Balance" />
             </div>
         </main>
         <Footer />
