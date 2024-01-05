9595a19606ec5b34640703d7a2f9ec8302273e13
axido545 <axido545@gmail.com>
2024-01-05 22:21:02 +0100

    add btntransaction

--- src/components/account/Account.jsx
+++ src/components/account/Account.jsx
@@ -1,9 +1,11 @@
 import "./account.css"
 import AccountDesc from "../accountdesc/accountDesc"
+import BtnTransaction from "../btntransaction/BtnTransaction"
 
 export default function Account({ title, amount, description }) {
     return <section className="account">
         <AccountDesc title={title} amount={amount} description={description} />
+        <BtnTransaction />
     </section >
 }
 
---
+++ src/components/btntransaction/BtnTransaction.jsx
@@ -0,0 +1,5 @@
+import "./btntransaction.css"
+
+export default function BtnTransaction() {
+    return <button className="btn-transaction">View transactions</button>
+}
