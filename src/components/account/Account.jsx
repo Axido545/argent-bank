8b93e0f785e6460902144a007fa85ed5283e1c4d
axido545 <axido545@gmail.com>
2024-01-09 22:46:25 +0100

    correction account & accountdesc

--- src/components/account/Account.jsx
+++ src/components/account/Account.jsx
@@ -1,12 +1,10 @@
 import "./account.css"
 import AccountDesc from "../accountdesc/accountDesc"
 import BtnTransaction from "../btntransaction/BtnTransaction"
-import { useSelector } from "react-redux";
 
 export default function Account({ type }) {
-    const account = useSelector((state) => state.account[type]);
     return <section className="account">
-        <AccountDesc title={account.title} amount={account.amount} description={account.description} />
+        <AccountDesc type={type} />
         <BtnTransaction />
     </section >
 }
--- src/components/accountdesc/accountDesc.jsx
+++ src/components/accountdesc/accountDesc.jsx
@@ -1,8 +1,8 @@
 import "./accountdesc.css"
 import { useSelector } from "react-redux";
 
-export default function AccountDesc() {
-    const account = useSelector((state) => state.account.checking);
+export default function AccountDesc({ type }) {
+    const account = useSelector((state) => state.account[type]);
     return <div className="account-content-wrapper">
         <h3 className="account-title">{account.title}</h3>
         <p className="account-amount">{account.amount}</p>
